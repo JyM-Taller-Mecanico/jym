f39a5cdfca78c8425b48ac5bb5c0f81c470de99f
Vannia543 <vaniacecilia42@gmail.com>
2024-01-03 19:24:54 -0300

    11111

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-let currentIndex = 0;
-const images = document.querySelectorAll('.carousel-image');
-const totalImages = images.length;
-
-document.getElementById('next').addEventListener('click', function () {
-    if (currentIndex < totalImages - 1) {
-        currentIndex++;
-    } else {
-        currentIndex = 0;
+let services = document.querySelector('.programas');
+let cards = [
+    {
+        title: 'Scanner',
+        img: './img/scanner.jpeg'
+    },
+    {
+        title: 'Cambio de aceite',
+        img: './img/Cambio_aceite.jpeg'
+    },
+    {
+        title: 'Aceites',
+        img: './img/aceites.jpeg'
     }
-    updateCarousel();
-});
+];
 
-document.getElementById('prev').addEventListener('click', function () {
-    if (currentIndex > 0) {
-        currentIndex--;
-    } else {
-        currentIndex = totalImages - 1;
-    }
-    updateCarousel();
-});
-
-function updateCarousel() {
-    const imageContainer = document.getElementById('image-container');
-    const newTransformValue = -currentIndex * 100 / totalImages + '%';
-    imageContainer.style.transform = 'translateX(' + newTransformValue + ')';
-}
-
+services.innerHTML = cards.map(card => {
+    return `
+        <div class="card">
+            <label class="card-title">
+                <h3>${card.title}</h3>
+            </label>
+            <img src="${card.img}" width="200" alt="${card.title}">
+        </div>
+    `;
+}).join('');
